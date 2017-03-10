2c399be8b82574acbaf3f7c4255a2aba29399993
zhangkang <zhangkang@lvmama.com>
2017-03-10 10:31:50 +0800

    简单路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,40 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {Routes, RouterModule} from "@angular/router";
+import { CrisisListComponent } from './component/crisis-list/crisis-list.component';
+import { HeroListComponent } from './component/hero-list/hero-list.component';
+import { HeroDetailComponent } from './component/hero-detail/hero-detail.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'hero/:id',      component: HeroDetailComponent },
+  {
+    path: 'heroes',
+    component: HeroListComponent,
+    data: { title: 'Heroes List' }
+  },
+  { path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroListComponent,
+    HeroDetailComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
