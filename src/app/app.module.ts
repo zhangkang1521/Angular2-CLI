bf9883c3fb7e578fcc3bdc5dba00240a8a698c0d
zhangkang <zhangkang@lvmama.com>
2017-03-19 17:36:26 +0800

    Jasmine测试

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MyListComponent } from './component/my-list/my-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyListComponent
   ],
   imports: [
     BrowserModule,
