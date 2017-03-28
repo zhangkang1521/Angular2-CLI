fa3dbc2b5aa41efc327162d80e709514a0f59b25
zhangkang <zhangkang@lvmama.com>
2017-03-28 18:52:22 +0800

    rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'app works!';
+
+  ngOnInit(): void {
+    // let button = document.querySelector('button');
+    // Observable.fromEvent(button, 'click')
+    //   .subscribe(() => console.log('clicked!'));
+    console.log('cold')
+    let obs = new Observable(observer => {
+      console.log('observable start')
+      observer.next(1);
+      observer.next(2);
+    });
+    console.log('start');
+    obs.subscribe(data => {
+      console.log(data);
+    });
+  }
+
+
 }
