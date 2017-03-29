a574aa9582b35d8e6df75d1353115af50cff52f4
zhangkang <zhangkang@lvmama.com>
2017-03-29 18:08:00 +0800

    rxjs （解密Angular2书上例子）

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,20 +11,110 @@ export class AppComponent implements OnInit{
   title = 'app works!';
 
   ngOnInit(): void {
-    // let button = document.querySelector('button');
-    // Observable.fromEvent(button, 'click')
-    //   .subscribe(() => console.log('clicked!'));
-    console.log('cold')
-    let obs = new Observable(observer => {
-      console.log('observable start')
-      observer.next(1);
-      observer.next(2);
+    // this.testConnect();
+    // this.testCreate();
+    // this.testMap();
+    // this.testForkJoin();
+    // this.testConcatMap();
+    this.testTimeout();
+  }
+
+  // 创建Observable
+  testCreate() {
+    let obs = Observable.create(val => {
+      val.next(1);
+      val.next(2);
+      val.complete();
     });
-    console.log('start');
+    obs.subscribe(data => {
+      console.log(data);
+    })
+  }
+
+  // 数据转换
+  testMap() {
+    let obs = Observable.create(val => {
+      val.next(1);
+      val.next(2);
+      val.complete();
+    });
+    obs.map(data => data * 10).subscribe(data => {
+      console.log(data);
+    })
+  }
+
+  // 组合
+  testForkJoin() {
+    let first = Observable.create(data => {
+      data.next(10);
+      data.complete();
+    });
+    let second = Observable.create(data => {
+      data.next(20);
+      data.complete();
+    });
+    let obs = Observable.forkJoin(first, second);
     obs.subscribe(data => {
       console.log(data);
     });
   }
+
+  // 第二个结果依赖第一个结果
+  testConcatMap() {
+    let first = Observable.create(data => {
+      data.next(10);
+      data.complete();
+    });
+    let createSecond = function (first) {
+      return Observable.create(data => {
+        data.next(first*2);
+        data.complete();
+      })
+    };
+    let obs = first.concatMap(first => {
+      return createSecond(first);
+    }).subscribe(second => {
+      console.log(second);
+    });
+    // method 2
+    // first.subscribe(first => {
+    //   createSecond(first).subscribe(second => {
+    //     console.log(second);
+    //   })
+    // })
+  }
+
+  testTimeout() {
+    // let obs = Observable.create(data => {
+    //   // data.delay(5000);
+    //   console.log('start');
+    //   data.next('hello');
+    //   data.complete();
+    // }).timer(5000);
+    let obs = Observable.timer(5000);
+    obs.timeout(6000).subscribe(data => {
+      console.log(data);
+    }, err => {
+      console.log(err);
+    })
+  }
+
+
+  // connect模式
+  testConnect(){
+    let obs = new Observable(observer => {
+      console.log('observer start');
+      observer.complete();
+    }).publish();
+    console.log('start');
+    obs.subscribe();
+    console.log('after subscribe');
+    obs.connect();
+  }
+
+
+
+
 
 
 }
